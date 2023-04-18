011fbe43af9ae512faec99fd68116573209e6bbe
pankajdkoli <pankajdkoli@gmail.com>
2023-04-18 22:59:25 +0530

    add local storage or css

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,32 @@ import React, { useState } from "react";
 import "./App.css";
 import InputTodo from "./components/InputTodo";
 import { Todo } from "./components/model";
+import "./components/TodoApp.css";
 
-const App = () => {
+function App() {
   const [todo, setTodo] = useState<string>("");
-
   const [todos, setTodos] = useState<Todo[]>([]);
+
+  // Define the updateTodo function with the correct parameter type
+  const updateTodo = (newTodo: Todo[]) => {
+    localStorage.setItem("todos", JSON.stringify(newTodo));
+    setTodos(newTodo);
+  };
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
-
     // have something in todo then goe next to setTodos
     if (todo) {
-      //get from todo model id todo isdone these are
-      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
-      // after done submit input file input filds goe empty
+      // Get the form input value and create a new Todo object get from todo model id todo isdone these are
+      const newTodo: Todo = {
+        id: Date.now(),
+        todo: todo,
+        isDone: false,
+      };
+      // Update the todos state variable with the newTodo object
+      updateTodo([...todos, newTodo]);
+
+      // Clear the form input form field
       setTodo("");
     }
   };
@@ -24,15 +36,30 @@ const App = () => {
   return (
     <>
       <InputTodo todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      <li className="todolist">
-        <span>{todo}</span>
-      </li>
+      <li className="todolist">{/* <span>{todo}</span> */}</li>
 
-      {todos.map((todoList) => {
+      {/* {todos.map((todoList) => {
         return <li key={todoList.id}>{todoList.todo}</li>;
-      })}
+      })} */}
+      <div className="container" id="main-content">
+        <ul className="todos">
+          Todo List
+          {todos.map((todoList) => {
+            return (
+              <li
+                key={todoList.id}
+                className={todoList.isDone ? "completed" : ""}
+              >
+                {todoList.todo}
+              </li>
+            );
+          })}
+        </ul>
+
+        <button className="list-button">check</button>
+      </div>
     </>
   );
-};
+}
 
 export default App;
--- src/components/InputTodo.tsx
+++ src/components/InputTodo.tsx
@@ -4,25 +4,27 @@ import "./TodoApp.css";
 interface InputTodoProps {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
-  handleAdd: (e: React.FormEvent) => void; // does not return any th
+  handleAdd: (e: React.FormEvent) => void; // void does not return any th
 }
 
-const InputTodo = ({ todo, setTodo, handleAdd }: InputTodoProps) => {
+function InputTodo({ todo, setTodo, handleAdd }: InputTodoProps) {
   return (
-    <header className="container" id="main-herder">
-      <h1>Today Task</h1>
-      <form id="todo-form" onSubmit={(e) => handleAdd(e)}>
-        <input
-          value={todo}
-          type="text"
-          name="todoItem"
-          className="task-name"
-          placeholder="do your task"
-          onChange={(e) => setTodo(e.target.value)}
-        />
-        <button type="submit"> Add Task </button>
-      </form>
-    </header>
+    <>
+      <header className="container" id="main-herder">
+        <h1>Today Task</h1>
+        <form id="todo-form" onSubmit={(e) => handleAdd(e)}>
+          <input
+            value={todo}
+            type="text"
+            name="todoItem"
+            className="task-name"
+            placeholder="do your task"
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button type="submit"> Add Task </button>
+        </form>
+      </header>
+    </>
   );
-};
+}
 export default InputTodo;
