508f939502e4cd1acdc3b413963d40c744c81931
pankajdkoli <pankajdkoli@gmail.com>
2023-04-18 15:07:08 +0530

    make changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import "./App.css";
 
 import Todo from "./components/todo";
 
-// import Test from "./components/test.jsx";
-
 function App() {
   return (
     <>
       <Todo></Todo>
-      {/* <Test /> */}
     </>
   );
 }
